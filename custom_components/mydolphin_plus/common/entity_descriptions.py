77cb21da5ddbace0c32b19ef2b1f0cf59aa8629e
Clément Notin <clement.notin@gmail.com>
2026-07-31 22:12:22 +0200

    Rename robot connectivity sensor

--- custom_components/mydolphin_plus/common/entity_descriptions.py
+++ custom_components/mydolphin_plus/common/entity_descriptions.py
@@ -34,9 +34,9 @@ from .clean_modes import (
 )
 from .consts import (
     DATA_KEY_AWS_BROKER,
-    DATA_KEY_ROBOT_CONNECTIVITY,
     DATA_KEY_BATTERY,
     DATA_KEY_CLEAN_MODE,
+    DATA_KEY_CONNECTIVITY,
     DATA_KEY_CYCLE_COUNT,
     DATA_KEY_CYCLE_TIME,
     DATA_KEY_CYCLE_TIME_LEFT,
@@ -245,11 +245,11 @@ ENTITY_DESCRIPTIONS: list[MyDolphinPlusEntityDescription] = [
         translation_key=slugify(DATA_KEY_AWS_BROKER),
     ),
     MyDolphinPlusBinarySensorEntityDescription(
-        key=slugify(DATA_KEY_ROBOT_CONNECTIVITY),
-        name=DATA_KEY_ROBOT_CONNECTIVITY,
+        key=slugify(DATA_KEY_CONNECTIVITY),
+        name=DATA_KEY_CONNECTIVITY,
         device_class=BinarySensorDeviceClass.CONNECTIVITY,
         entity_category=EntityCategory.DIAGNOSTIC,
-        translation_key=slugify(DATA_KEY_ROBOT_CONNECTIVITY),
+        translation_key=slugify(DATA_KEY_CONNECTIVITY),
     ),
     MyDolphinPlusSensorEntityDescription(
         key=slugify(DATA_KEY_ROBOT_ERROR),
